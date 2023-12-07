b1334a3e3ef10194f4af99191e4690a857f38f7d
jasanbadell <jasanbadelldev@gmail.com>
2023-12-07 11:28:37 -0500

    First Commit

---
+++ js/configuration.typed.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".typed", {
+  strings: [
+    `<b class="text-green">Front-End</b>`,
+    `<b class="text-green"> Web Designer</b>`,
+    `<b class="text-green">Web Developer</b>`,
+  ],
+  stringsElement: "#cadenas-texto",
+  typeSpeed: 75,
+  startDelay: 300,
+  backSpeed: 75,
+  SmartBackspace: true,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorBar: "|",
+  contentType: "html",
+});
